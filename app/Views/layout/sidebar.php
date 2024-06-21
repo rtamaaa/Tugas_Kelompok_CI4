8cabd4ba427fe56aa2121b33be0b85ad35c4ba54
salwasyahratul <149489570+salwasyahratul@users.noreply.github.com>
2024-06-21 23:11:30 +0700

    Update sidebar.php

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -2,8 +2,8 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?=base_url('adminlte/index3.html')?>"  class="brand-link">
-      <img src="<?=base_url('adminlte/distt/img/AdminLTELogo.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">STIM</span>
+      <img src="<?=base_url('adminlte/dist/img/AdminLTELogo.png')?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">GO-blog</span>
     </a>
 
     <!-- Sidebar -->
@@ -11,7 +11,7 @@
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?=base_url('adminlte/distt/img/user2-160x160.jpg')?>" class="img-circle elevation-2" alt="User Image">
+          <img src="<?=base_url('adminlte/dist/img/user2-160x160.jpg')?>" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Admin</a>
@@ -45,26 +45,20 @@
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                  <a href="<?= base_url('mahasiswa/index') ?>" class="nav-link <?= strpos(current_url(), 'dosen') !== false ? 'active' : '' ?>">
+                  <a href="<?= base_url('content/') ?>" class="nav-link <?= strpos(current_url(), 'dosen') !== false ? 'active' : '' ?>">
                       <i class="far fa-circle nav-icon"></i>
-                      <p>Mahasiswa</p>
+                      <p>Content</p>
                   </a>
               </li>
               <li class="nav-item">
-                <a href="<?=base_url('Prodi/index')?>" class="nav-link">
+                <a href="<?=base_url('user')?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Prodi</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="../tables/jsgrid.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Fakultas</p>
+                  <p>User</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item menu-close">
+          <!-- <li class="nav-item menu-close">
             <a href="#" class="nav-link ">
               <i class="nav-icon fas fa-table"></i>
               <p>
@@ -86,7 +80,7 @@
                 </a>
               </li>
             </ul>
-          </li>
+          </li> -->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
